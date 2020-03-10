d9134b5c1a59135967874a0a9ed01f6af2a90342
nils <nils@gangshi>
2020-03-10 02:02:50 -0400

    added ALL mode. added map of mode to tools. might not need this, or need to flip it, eventually

--- src/constants/mode-tools.js
+++ src/constants/mode-tools.js
@@ -1,8 +1,9 @@
 // src/js/constants/modes-tooli.js
-import { EDGE, SHAPE, SPRITE, VERTEX } from 'constants/modes';
+import { ALL, EDGE, SHAPE, SPRITE, VERTEX } from 'constants/modes';
 import { ADD, DELETE, SELECT, ROTATE } from 'constants/tools';
 
 export default {
+  [ALL]: [ADD, DELETE, SELECT, ROTATE],
   [EDGE]: [DELETE, SELECT, ROTATE],
   [SHAPE]: [DELETE, SELECT, ROTATE],
   [SPRITE]: [DELETE, SELECT, ROTATE],
--- src/constants/modes.js
+++ src/constants/modes.js
@@ -1,10 +1,12 @@
 // src/js/constants/modes.js
+export const ALL = 'ALL';
 export const EDGE = 'EDGE';
 export const SHAPE = 'SHAPE';
 export const SPRITE = 'SPRITE';
 export const VERTEX = 'VERTEX';
 
 export default {
+    ALL,
     EDGE,
     SHAPE,
     SPRITE,
