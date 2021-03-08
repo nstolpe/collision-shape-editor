050efdebda6c3895e642e4445ad02bc21e2411b6
nils <nils@gangshi>
2021-03-08 02:03:44 -0500

    updated react deps. dropped color-picker until rest and it are updated

--- src/components/html/Controls.jsx
+++ src/components/html/Controls.jsx
@@ -22,7 +22,7 @@ import VertexIcon from 'components/html/VertexIcon';
 import Separator from 'components/html/Separator';
 import RootContext from 'contexts/RootContext';
 import { property } from 'tools/property';
-// import ColorPicker from 'color-picker/dist/index.js';
+//import ColorPicker from 'color-picker';
 import ColorPicker from 'components/html/color-picker/components/ColorPicker';
 
 const ControlWrapper = styled.div`
