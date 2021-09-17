c477d10d215944657ef7ddc9f86f16340814f744
nils <nils@gangshi>
2021-09-16 20:57:44 -0400

    many updates

--- src/components/pixi/InteractiveViewport.jsx
+++ src/components/pixi/InteractiveViewport.jsx
@@ -1,7 +1,9 @@
 // src/js/components/pixi/InteractiveViewport.js
 import * as PIXI from 'pixi.js';
 import React, {
+  useCallback,
   useEffect,
+  useLayoutEffect,
   useRef,
   useState,
 } from 'react';
@@ -11,16 +13,84 @@ import {
   addSprite,
   removeTextureSource,
   scaleUI,
+  setAltPressed,
+  setCtrlPressed,
+  setShiftPressed,
 } from 'actions/actions';
+import withSelector from 'components/hoc/withSelector';
+import Geometry from 'components/pixi/Geometry';
+import Rectangle from 'components/pixi/base/Rectangle';
+import Sprites from 'components/pixi/Sprites';
+import Viewport from 'components/pixi/base/Viewport';
+import { GRAB, GRABBING, POINTER } from 'constants/cursors';
 import * as Modes from 'constants/modes';
 import ScreenContext from 'contexts/ScreenContext';
-import withSelector from 'components/hoc/withSelector';
-import Viewport from 'components/pixi/base/Viewport';
-import Edges from 'components/pixi/Edges';
-import Sprites from 'components/pixi/Sprites';
-import Geometry from 'components/pixi/Geometry';
-import { GRAB, GRABBING } from 'constants/cursors';
+import useOverlayRef from 'hooks/useOverlayRef';
 import usePointerInteractions from 'hooks/usePointerInteractions';
+
+/**
+ * Compares all arguments for strict equality
+ */
+const allEqual = (...elements) => elements.reduce(
+  (areEqual, element, index) => areEqual && element === elements[(index + 1) % elements.length],
+  true
+);
+
+/**
+ * Adds global keyboard shortcuts to `document`.
+ *
+ * @param {function} dispatch  Dispatch function to call actions with.
+ */
+const useKeyboardShortcuts = dispatch => {
+  useEffect(() => {
+    const onKeyDown = ({ key, keyCode }) => {
+      switch (true) {
+        case key === 'Control' || keyCode === 17:
+          console.log('control pressed');
+          dispatch(setCtrlPressed(true));
+          break;
+        case key === 'Alt' || keyCode === 18:
+          console.log('alt pressed');
+          dispatch(setAltPressed(true));
+          break;
+        case key === 'Shift' || keyCode === 16:
+          console.log('shift pressed');
+          dispatch(setShiftPressed(true));
+          break;
+        default:
+          /* @TODO log key and keyCode if debugging? (once that's set up)*/
+      }
+    };
+
+    const onKeyUp = ({ key, keyCode }) => {
+      if (key === 'Control' || keyCode === 17) {
+        dispatch(setCtrlPressed(false));
+        console.log('control released');
+      } else if (key === 'Alt' || keyCode === 18) {
+        dispatch(setAltPressed(false));
+        console.log('alt released');
+      } else if (key === 'Shift' || keyCode === 16) {
+        dispatch(setShiftPressed(false));
+        console.log('shift released');
+      }
+    };
+
+    document.addEventListener('keydown', onKeyDown);
+    document.addEventListener('keyup', onKeyUp);
+
+    return () => {
+      document.removeEventListener('keydown', onKeyDown);
+      document.removeEventListener('keyup', onKeyUp);
+    };
+  }, [dispatch]);
+};
+
+const getCursor = ({ mode, tool, altPressed, ctrlPressed, shiftPressed }) => {
+  switch (true) {
+    default:
+      return POINTER;
+  }
+};
 
 const selector = ({
   dispatch,
@@ -30,6 +100,13 @@ const selector = ({
   textureSources,
   scale,
   vertices,
+  selectOverlay: {
+    enabled: selectOverlayEnabled,
+    height: selectOverlayHeight,
+    width: selectOverlayWidth,
+    x: selectOverlayX,
+    y: selectOverlayY,
+  },
 }) => ({
   dispatch,
   backgroundColor,
@@ -38,15 +115,51 @@ const selector = ({
   textureSources,
   scale,
   vertices,
+  selectOverlayEnabled,
+  selectOverlayHeight,
+  selectOverlayWidth,
+  selectOverlayX,
+  selectOverlayY,
 });
 
 const comparator = (
-  { scale, vertices, ...restProps},
-  { scale: oldScale, vertices: oldVertices, ...restOldProps }
+  {
+    scale,
+    vertices,
+    selectOverlay,
+    selectOverlayEnabled,
+    selectOverlayHeight,
+    selectOverlayWidth,
+    selectOverlayX,
+    selectOverlayY,
+    ...restProps
+  },
+  {
+    scale: oldScale,
+    vertices: oldVertices,
+    selectOverlay: oldSelectOverlay,
+    selectOverlayEnabled: oldSelectOverlayEnabled,
+    selectOverlayHeight: oldSelectOverlayHeight,
+    selectOverlayWidth: oldSelectOverlayWidth,
+    selectOverlayX: oldSelectOverlayX,
+    selectOverlayY: oldSelectOverlayY,
+    ...restOldProps
+  }
 ) => {
   if (
     (scale?.x ?? scale?.[0] ?? scale) !== (oldScale?.x ?? oldScale?.[0] ?? oldScale) ||
     (scale?.y ?? scale?.[1] ?? scale) !== (oldScale?.y ?? oldScale?.[1] ?? oldScale)
+  ) {
+    return false;
+  }
+
+  // @TODO move the selector overlay into it's own component, if possible.
+  if (
+    oldSelectOverlayEnabled !== selectOverlayEnabled ||
+    oldSelectOverlayHeight !== selectOverlayHeight ||
+    oldSelectOverlayWidth !== selectOverlayWidth ||
+    oldSelectOverlayX !== selectOverlayX ||
+    oldSelectOverlayY !== selectOverlayY
   ) {
     return false;
   }
@@ -56,8 +169,8 @@ const comparator = (
   }
 
   for (let i = 0, l = vertices?.length; i < l; i++) {
-    const vertex = vertices[i];
-    const oldVertex = oldVertices[i];
+    const vertex = vertices.index(i);
+    const oldVertex = oldVertices.index(i);
 
     if (
       vertex.name !== oldVertex.name ||
@@ -68,11 +181,8 @@ const comparator = (
     }
   }
 
-  for (let i = 0, l = restProps.length; i < l; i++) {
-    const prop = restProps[i];
-    const oldProp = restOldProps[i];
-
-    if (prop !== oldProp) {
+  for (let key of Object.keys(restProps)) {
+    if (restProps[key] !== restOldProps[key]) {
       return false;
     }
   }
@@ -95,10 +205,17 @@ const InteractiveViewport = props => {
     screenHeight,
     screenWidth,
     vertices,
+    // @TODO move overlay to own component
+    selectOverlayEnabled,
+    selectOverlayHeight,
+    selectOverlayWidth,
+    selectOverlayX,
+    selectOverlayY,
     ...restProps
   } = props;
-  const viewport = useRef(null);
-  const { loader, renderer } = usePixiApp();
+  const pixiApp = usePixiApp();
+  const { loader, renderer, ticker } = pixiApp;
+  window.ticker = window.ticker ?? pixiApp.ticker;
   const { plugins: { interaction } } = renderer;
   const [cursor, setCursor] = useState(GRAB);
   const onPointerDown = event => {
@@ -111,17 +228,22 @@ const InteractiveViewport = props => {
     handlePointerMove,
     handlePointerUp,
     selectedVertices,
-  } = usePointerInteractions(dispatch, tool, vertices);
+  } = usePointerInteractions(dispatch, tool, vertices, scale);
   const onZoomed = ({
     viewport: {
       scale: { x, y },
     },
   }) => dispatch(scaleUI({ x, y }));
-  // const {
-  //   textureSources = [],
-  // } = props;
-  // const context = useContext(ScreenContext);
-  // renderer.plugins.interaction.moveWhenInside = true;
+
+  const viewportRef = useOverlayRef();
+  const [viewportBackgroundProps, setViewportBackgroundProps] = useState({
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+  });
+  const [backgroundPropsSet, setBackgroundPropsSet] = useState(false);
+
   useEffect(
     () => {
       textureSources.forEach(textureSource => {
@@ -154,16 +276,19 @@ const InteractiveViewport = props => {
     ]
   );
 
+  useKeyboardShortcuts(dispatch);
+
   return (
     <Viewport
-      name="Interactive Viewport"
-      ref={viewport}
+      name="VIEWPORT"
+      ref={viewportRef}
       drag={{ keyToPress: 'ControlLeft' }}
       pinch={{ percent: 1 }}
       wheel={{ percent: 0.05 }}
       onzoomed={onZoomed}
       interaction={interaction}
-      cursor={cursor}
+      // cursor={cursor}
+      cursor={getCursor({ mode, tool })}
       pointerdown={onPointerDown}
       pointerup={onPointerUp}
       screenHeight={screenHeight}
@@ -171,8 +296,62 @@ const InteractiveViewport = props => {
       pointerdown={handlePointerDown}
       pointerup={handlePointerUp}
       pointermove={handlePointerMove}
+      onmovedend={viewport => {
+        const { top, bottom, left, right } = viewport;
+
+        setViewportBackgroundProps(
+          currentViewportBackgroundProps => ({
+              ...currentViewportBackgroundProps,
+              ...{
+                x: left,
+                y: top,
+                width: right - left,
+                height: bottom - top,
+              },
+          })
+        );
+      }}
+      onmoved={({ viewport }) => {
+        const { top, bottom, left, right } = viewport;
+
+        setViewportBackgroundProps(
+          currentViewportBackgroundProps => ({
+              ...currentViewportBackgroundProps,
+              ...{
+                x: left,
+                y: top,
+                width: right - left,
+                height: bottom - top,
+              },
+          })
+        );
+      }}
+      onframeend={viewport => {
+        const { top, bottom, left, right } = viewport;
+
+        if (!backgroundPropsSet && !allEqual(top, bottom, 0) && !allEqual(left, right, 0)) {
+          setViewportBackgroundProps(
+            currentViewportBackgroundProps => ({
+                ...currentViewportBackgroundProps,
+                ...{
+                  x: left,
+                  y: top,
+                  width: right - left,
+                  height: bottom - top,
+                },
+            })
+          );
+
+          setBackgroundPropsSet(true);
+        }
+      }}
       {...restProps}
     >
+      <Rectangle
+        {...viewportBackgroundProps}
+        fill={backgroundColor}
+        name={'BACKGROUND'}
+      />
       <Sprites />
       <Geometry
         interactive={mode === Modes.VERTEX}
--- src/components/pixi/Screen.jsx
+++ src/components/pixi/Screen.jsx
@@ -1,44 +1,21 @@
 // src/js/components/pixi/Screen.js
+import * as PIXI from 'pixi.js';
 import PropTypes from 'prop-types';
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { Stage } from 'react-pixi-fiber';
 
 import { useRootContext } from 'contexts/RootContext';
 import ScreenContext from 'contexts/ScreenContext';
+import {
+  setAltPressed,
+  setCtrlPressed,
+  setShiftPressed,
+} from 'actions/actions';
 import InteractiveViewport from 'components/pixi/InteractiveViewport';
+import Rectangle from 'components/pixi/base/Rectangle';
 
 const Screen = ({ children, width, height }) => {
   const state = useRootContext();
-
-  useEffect(() => {
-    const onKeyDown = ({ key, keyCode }) => {
-      if (key === 'Control' || keyCode === 17) {
-        console.log('control pressed');
-      } else if (key === 'Alt' || keyCode === 18) {
-        console.log('alt pressed');
-      } else if (key === 'Shift' || keyCode === 16) {
-        console.log('shift pressed');
-      }
-    };
-
-    const onKeyUp = ({ key, keyCode }) => {
-      if (key === 'Control' || keyCode === 17) {
-        console.log('control released');
-      } else if (key === 'Alt' || keyCode === 18) {
-        console.log('alt released');
-      } else if (key === 'Shift' || keyCode === 16) {
-        console.log('shift released');
-      }
-    };
-
-    document.addEventListener('keydown', onKeyDown);
-    document.addEventListener('keyup', onKeyUp);
-
-    return () => {
-      document.removeEventListener('keydown', onKeyDown);
-      document.removeEventListener('keyup', onKeyUp);
-    };
-  }, []);
 
   return (
     <Stage
@@ -46,11 +23,12 @@ const Screen = ({ children, width, height }) => {
         console.log(e.type);
         e.preventDefault();
       }}
+      name="SCREEN"
       options={
         {
           width,
           height,
-          backgroundColor: state.backgroundColor,
+          // backgroundColor: state.backgroundColor,
           autoResize: true,
           resolution: state.resolution,
           antialias: true,
