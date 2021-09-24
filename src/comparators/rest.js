e0a28c4af58a6332b2eac3fd1604f78f7da6f12a
nils <nils@gangshi>
2021-09-24 02:44:40 -0400

    removed log

--- src/comparators/rest.js
+++ src/comparators/rest.js
@@ -2,7 +2,6 @@
 const comparator = (props, oldProps) => {
   for (let key of Object.keys(props)) {
     if (props[key] !== oldProps[key]) {
-      console.log(key, 'key')
       return false;
     }
   }
