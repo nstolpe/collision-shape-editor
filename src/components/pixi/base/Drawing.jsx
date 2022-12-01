0422694018d231acd55703e77b5caf9f5de2a630
nils <nils@gangshi>
2022-12-01 01:24:25 -0500

    commented out customWillDetach since it was causing an error. lost of comments, might be worth looking into more

--- src/components/pixi/base/Drawing.jsx
+++ src/components/pixi/base/Drawing.jsx
@@ -7,6 +7,10 @@ const TYPE = 'Drawing';
 
 export const behavior = {
   customDisplayObject: (props) => new PIXI.Graphics(),
+  customDidAttach: (graphicsInstance) => {
+    // set based on prop
+    // graphicsInstance.cacheAsBitmap = true;
+  },
   customApplyProps: function (graphicsInstance, oldProps, newProps) {
     let oldPropsFinal;
     let newPropsFinal;
@@ -36,7 +40,18 @@ export const behavior = {
 
     this.applyDisplayObjectProps(oldPropsFinal, newPropsFinal);
   },
-  customWillDetach: (graphicsInstance) => graphicsInstance.destroy(),
+  // graphicsInstance.destroy() is already being called somewhere else,
+  // so this is causing an error because internally this is getting more then necessary:
+  // this._geometry.refCount--
+  // 2nd time through there is no _geometry on this, so error
+  // other destroy call is from a removeChild call from the Drawing's parent:
+  // https://github.com/michalochman/react-pixi-fiber/blob/1e6014427e8e7859a60fac1b74b3e6a330d396eb/src/ReactPixiFiber.js#L35-L43
+  // Maybe there should be a check for `if (!_child_destroyed)` around the `child.destroy` call,
+  // in case a custom component doesn't have a parent to destroy it of for some other reason wants to call destroy?
+  // customWillDetach: (graphicsInstance) => {
+  //   console.log(graphicsInstance._destroyed);
+  //   graphicsInstance.destroy();
+  // },
 };
 
 export default CustomPIXIComponent(behavior, TYPE);
