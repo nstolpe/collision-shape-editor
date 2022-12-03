8dc14371018ddf845866cdcee117df02f1a283ae
nils <nils@gangshi>
2022-12-02 23:51:35 -0500

    formatting and commented unused vars

--- src/reducers/helpers/get-shape-vertices-relative-to-coordinates.js
+++ src/reducers/helpers/get-shape-vertices-relative-to-coordinates.js
@@ -8,32 +8,37 @@ import { DEFAULT_DELIMITER } from 'Utility/prefix';
  * recreates a List of shapes, updating the coordinates of shapes vertices based
  * on their distance from coordinates
  */
-const getShapeVerticesRelativeToCoordinates = (selectedVertices, coordinates, shapes) => {
+const getShapeVerticesRelativeToCoordinates = (
+  selectedVertices,
+  coordinates,
+  shapes
+) => {
   let newShapes = new List(shapes, shapes.keys);
 
   for (const name in selectedVertices) {
     const [, vertexKey, , shapeKey] = name.split(DEFAULT_DELIMITER);
     const shape = newShapes.key(shapeKey);
-    const vertex = shape.vertices.key(vertexKey);
-    const vertexIndex = shape.vertices.keys.indexOf(vertexKey);
+    // const vertex = shape.vertices.key(vertexKey);
+    // const vertexIndex = shape.vertices.keys.indexOf(vertexKey);
     const distance = selectedVertices[name];
-    const newVertices = shape.vertices.map(
-      (vertex, index, key) => {
-        if (key === vertexKey) {
-          return {
-            x: coordinates.x + distance.x,
-            y: coordinates.y + distance.y,
-          };
-        } else {
-          return vertex;
-        }
+    const newVertices = shape.vertices.map((vertex, index, key) => {
+      if (key === vertexKey) {
+        return {
+          x: coordinates.x + distance.x,
+          y: coordinates.y + distance.y,
+        };
+      } else {
+        return vertex;
       }
-    );
+    });
 
-    newShapes = newShapes.setByKey({
-      ...shape,
-      vertices: newVertices,
-    }, shapeKey);
+    newShapes = newShapes.setByKey(
+      {
+        ...shape,
+        vertices: newVertices,
+      },
+      shapeKey
+    );
   }
 
   return newShapes;
