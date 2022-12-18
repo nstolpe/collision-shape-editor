fd719b62a6ee10604ba4533253a62237640b2e7e
nils <nils@gangshi>
2022-12-18 13:38:29 -0500

    remove portfinder stuff and pull port from env since it gets set in the pre script

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,64 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const portfinder = require('portfinder');
 
-const DEFAULT_PORT = 3000;
-
-module.exports = async () => {
-  const port = await portfinder.getPortPromise({ port: DEFAULT_PORT });
-
-  return {
-    context: path.resolve(__dirname, 'src'),
-    entry: './index.jsx',
-    output: {
-      path: path.join(__dirname, '/dist'),
-      filename: 'bundle.js',
-    },
-    devServer: {
-      port,
-    },
-    devtool: 'source-map',
-    module: {
-      rules: [
-        {
-          test: /\.mjs$/,
-          include: /node_modules/,
-          type: 'javascript/auto',
-        },
-        {
-          test: /\.jsx?$/,
-          exclude: '/node_modules/',
-          loader: 'babel-loader',
-        },
-        {
-          test: /\.glsl$/,
-          loader: 'webpack-glsl-loader',
-        },
-        {
-          test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
-          type: 'asset/resource',
-        },
-      ],
-    },
-    resolve: {
-      alias: {
-        Fonts: path.resolve(__dirname, 'src/fonts'),
-        Actions: path.resolve(__dirname, 'src/actions'),
-        Comparators: path.resolve(__dirname, 'src/comparators'),
-        Components: path.resolve(__dirname, 'src/components'),
-        Constants: path.resolve(__dirname, 'src/constants'),
-        Contexts: path.resolve(__dirname, 'src/contexts'),
-        Hooks: path.resolve(__dirname, 'src/hooks'),
-        Reducers: path.resolve(__dirname, 'src/reducers'),
-        Selectors: path.resolve(__dirname, 'src/selectors'),
-        Shaders: path.resolve(__dirname, 'src/shaders'),
-        Store: path.resolve(__dirname, 'src/store'),
-        Utility: path.resolve(__dirname, 'src/utility'),
+module.exports = (env) => ({
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.jsx',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    port: process.env['PORT'],
+    open: false,
+  },
+  devtool: 'source-map',
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto',
       },
-      extensions: ['.js', '.jsx', '.glsl'],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: path.join(__dirname, '/public/index.html'),
-      }),
+      {
+        test: /\.jsx?$/,
+        exclude: '/node_modules/',
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.glsl$/,
+        loader: 'webpack-glsl-loader',
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+        type: 'asset/resource',
+      },
     ],
-  };
-};
+  },
+  resolve: {
+    alias: {
+      Fonts: path.resolve(__dirname, 'src/fonts'),
+      Actions: path.resolve(__dirname, 'src/actions'),
+      Comparators: path.resolve(__dirname, 'src/comparators'),
+      Components: path.resolve(__dirname, 'src/components'),
+      Constants: path.resolve(__dirname, 'src/constants'),
+      Contexts: path.resolve(__dirname, 'src/contexts'),
+      Hooks: path.resolve(__dirname, 'src/hooks'),
+      Reducers: path.resolve(__dirname, 'src/reducers'),
+      Selectors: path.resolve(__dirname, 'src/selectors'),
+      Shaders: path.resolve(__dirname, 'src/shaders'),
+      Store: path.resolve(__dirname, 'src/store'),
+      Utility: path.resolve(__dirname, 'src/utility'),
+    },
+    extensions: ['.js', '.jsx', '.glsl'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, '/public/index.html'),
+      ENV: JSON.stringify(env),
+    }),
+  ],
+});
