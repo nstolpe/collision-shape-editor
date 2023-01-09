34e13ab4ac2c23cd8e87a709d20440dcddb97bc7
nils <nils@gangshi>
2023-01-08 19:24:21 -0500

    REMOVE -> DELETE

--- src/constants/tools.js
+++ src/constants/tools.js
@@ -1,7 +1,7 @@
 // src/js/constants/tools.js
 
 export const ADD = 'ADD';
-export const DELETE = 'REMOVE';
+export const DELETE = 'DELETE';
 export const MOVE = 'MOVE';
 export const ROTATE = 'ROTATE';
 export const SELECT = 'SELECT';
