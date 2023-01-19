edb065356d4f433623cfc7e973cc7b4cf76ce0d3
nils <nils@gangshi>
2023-01-19 04:15:49 -0500

    removed some unnecessary code and memo-ized the vertices and edges since geomtry is rerendering and that calculation is being called all the time

--- src/components/pixi/Geometry.jsx
+++ src/components/pixi/Geometry.jsx
@@ -14,7 +14,7 @@ import { EDGE, SHAPE, VERTEX } from 'Constants/prefixes';
 import { SELECT } from 'Constants/tools';
 import ScreenContext from 'Contexts/ScreenContext';
 import { addPrefix, removePrefix, DEFAULT_DELIMITER } from 'Utility/prefix';
-window.chroma = chroma;
+
 const selector = ({ backgroundColor, scale, tool }) => ({
   backgroundColor,
   scale,
@@ -55,106 +55,118 @@ const Geometry = ({
     [scale.x, scale.y]
   );
 
-  const containerRef = useCallback((container) => {
-    if (container) {
-      const { height, width, x, y } = container.getBounds();
+  const components = useMemo(
+    () =>
+      Array.from(vertices.entries()).reduce(
+        (result, [vertex1Index, vertex1Key, vertex1]) => {
+          const vertex1Id = `${addPrefix(
+            VERTEX,
+            vertex1Key
+          )}${DEFAULT_DELIMITER}${name}`;
+          const { x: x1, y: y1 } = vertex1;
+          const vertexProps = {
+            id: vertex1Id,
+            key: vertex1Id,
+            scale: inverseScale,
+            selected: Object.prototype.hasOwnProperty.call(
+              selectedVertices,
+              vertex1Id
+            ),
+            tool,
+            x: x1,
+            y: y1,
+          };
 
-      container.pivot.set(x + width / 2, y + height / 2);
-      container.position.set(x + width / 2, y + height / 2);
-    }
-  }, []);
+          // draw the final edge if the geometry is closed.
+          if (closed || vertex1Index !== vertices.length - 1) {
+            const vertex2Index = (vertex1Index + 1) % vertices.length;
+            const vertex2 = vertices.index(vertex2Index);
+            const vertex2Key = vertices.keys[vertex2Index];
+            const vertex2Id = `${addPrefix(
+              VERTEX,
+              vertex2Key
+            )}${DEFAULT_DELIMITER}${name}`;
+            const edgeId = addPrefix(
+              EDGE,
+              `${vertex1Key}${DEFAULT_DELIMITER}${vertex2Key}${DEFAULT_DELIMITER}${name}`
+            );
+            const { x: x2, y: y2 } = vertex2;
+            const dx = (x2 - x1) * scale.x;
+            const dy = (y2 - y1) * scale.y;
+            const edgeProps = {
+              id: edgeId,
+              key: edgeId,
+              length: Math.hypot(dx, dy), //Math.sqrt(dx ** 2 + dy ** 2),
+              rotation: Math.atan2(dy, dx),
+              scale: inverseScale,
+              selected:
+                Object.prototype.hasOwnProperty.call(
+                  selectedVertices,
+                  vertex1Id
+                ) &&
+                Object.prototype.hasOwnProperty.call(
+                  selectedVertices,
+                  vertex2Id
+                ),
+              tool,
+              x1,
+              y1,
+              x2,
+              y2,
+            };
 
-  // const hitArea = useMemo(() => new PIXI.Polygon(vertices.values), [vertices]);
+            result[0].push(<Edge {...edgeProps} />);
+          }
 
-  const components = Array.from(vertices.entries()).reduce(
-    (result, [vertex1Index, vertex1Key, vertex1]) => {
-      const vertex1Id = `${addPrefix(
-        VERTEX,
-        vertex1Key
-      )}${DEFAULT_DELIMITER}${name}`;
-      const { x: x1, y: y1 } = vertex1;
-      const vertexProps = {
-        id: vertex1Id,
-        key: vertex1Id,
-        scale: inverseScale,
-        selected: selectedVertices.hasOwnProperty(vertex1Id),
-        tool,
-        x: x1,
-        y: y1,
-      };
+          result[1].push(<Vertex {...vertexProps} />);
 
-      // draw the final edge if the geometry is closed.
-      if (closed || vertex1Index !== vertices.length - 1) {
-        const vertex2Index = (vertex1Index + 1) % vertices.length;
-        const vertex2 = vertices.index(vertex2Index);
-        const vertex2Key = vertices.keys[vertex2Index];
-        const vertex2Id = `${addPrefix(
-          VERTEX,
-          vertex2Key
-        )}${DEFAULT_DELIMITER}${name}`;
-        const edgeId = addPrefix(
-          EDGE,
-          `${vertex1Key}${DEFAULT_DELIMITER}${vertex2Key}${DEFAULT_DELIMITER}${name}`
-        );
-        const { x: x2, y: y2 } = vertex2;
-        const dx = (x2 - x1) * scale.x;
-        const dy = (y2 - y1) * scale.y;
-        const edgeProps = {
-          id: edgeId,
-          key: edgeId,
-          length: Math.hypot(dx, dy), //Math.sqrt(dx ** 2 + dy ** 2),
-          rotation: Math.atan2(dy, dx),
-          scale: inverseScale,
-          selected:
-            selectedVertices.hasOwnProperty(vertex1Id) &&
-            selectedVertices.hasOwnProperty(vertex2Id),
-          tool,
-          x1,
-          y1,
-          x2,
-          y2,
-        };
+          if (showWinding) {
+            const [r, g, b] = chroma(
+              backgroundColor.toString(16).padStart(6, 0)
+            ).rgb();
+            const inverseHex = chroma({
+              r: 255 - r,
+              g: 255 - g,
+              b: 255 - b,
+            }).hex();
 
-        result[0].push(<Edge {...edgeProps} />);
-      }
+            result[2].push(
+              <Text
+                key={addPrefix('VERTEX_INDEX', vertex1Key)}
+                text={vertex1Index}
+                x={x1}
+                y={y1}
+                style={{
+                  fontFamily: 'Fira Mono',
+                  fontSize: '2rem',
+                  fill: inverseHex,
+                  padding: 4,
+                }}
+                scale={inverseScale}
+              />
+            );
+          }
 
-      result[1].push(<Vertex {...vertexProps} />);
-
-      if (showWinding) {
-        const [r, g, b] = chroma(
-          backgroundColor.toString(16).padStart(6, 0)
-        ).rgb();
-        const inverseHex = chroma({ r: 255 - r, g: 255 - g, b: 255 - b }).hex();
-
-        result[2].push(
-          <Text
-            key={addPrefix('VERTEX_INDEX', vertex1Key)}
-            text={vertex1Index}
-            x={x1}
-            y={y1}
-            style={{
-              fontFamily: 'Fira Mono',
-              fontSize: '2rem',
-              fill: inverseHex,
-              padding: 4,
-            }}
-            scale={inverseScale}
-          />
-        );
-      }
-
-      return result;
-    },
-    showWinding ? [[], [], []] : [[], []]
+          return result;
+        },
+        showWinding ? [[], [], []] : [[], []]
+      ),
+    [
+      backgroundColor,
+      closed,
+      inverseScale,
+      name,
+      scale.x,
+      scale.y,
+      selectedVertices,
+      showWinding,
+      tool,
+      vertices,
+    ]
   );
 
   return (
-    <Container
-      name={name}
-      ref={containerRef}
-      interactive
-      // hitArea={hitArea}
-    >
+    <Container name={name} interactive>
       {components}
     </Container>
   );
